e95c6201bcb2bd83056a11aaa1655df7b789bad0
Dj <mrdhiraj@gmail.com>
2016-07-31 23:25:37 +0545

    implent increament feature

--- ReversePocoGenerationSample/ReversePocoGenerationSample/Data/CustomDbContext.cs
+++ ReversePocoGenerationSample/ReversePocoGenerationSample/Data/CustomDbContext.cs
@@ -1,12 +1,56 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ReversePocoGenerationSample.Data
 {
     public class CustomDbContext : DbContext
     {
-        protected CustomDbContext(DbCompiledModel model) : base(model)
+        protected int SaveChangesCount { get; private set; }
+
+        public CustomDbContext(System.Data.Common.DbConnection existingConnection, bool contextOwnsConnection)
+            
         {
+            
+        }
+
+        public CustomDbContext(System.Data.Common.DbConnection existingConnection, System.Data.Entity.Infrastructure.DbCompiledModel model, bool contextOwnsConnection)
+             
+        {
+            
+        }
+
+        protected CustomDbContext(string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        {
+        }
+
+        protected CustomDbContext(string nameOrConnectionString, DbCompiledModel model)
+            : base(nameOrConnectionString, model)
+        {
+        }
+
+        public override int SaveChanges()
+        {
+            Console.WriteLine("SaveChanges()");
+            ++SaveChangesCount;
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync()
+        {
+            Console.WriteLine("SaveChangesAsync()");
+            ++SaveChangesCount;
+            return base.SaveChangesAsync();
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("SaveChangesAsync()");
+            ++SaveChangesCount;
+            return base.SaveChangesAsync(cancellationToken);
         }
     }
 }
